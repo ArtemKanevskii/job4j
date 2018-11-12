f83bc4d693a14cb2284a265921910790602bffc4
ArtemKanevskii <nwoa@mail.ru>
2018-11-12 15:25:00 +0200

    1.3. JavaDoc. Code convention[#85216]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,18 @@
  package ru.job4j.calculate;
- 
+ /**
+ * Class Calculate 
+ * 
+ * Вывод заданной строки в консоль.
+ * @author Artem Kanevskii
+ * @since 12.11.2018
+ * @version 0.1
+ */
  public class Calculate {
-	 
+ /**
+ * @param args - args.
+ */	 
 	 public static void main(String[] args) {
+
 		 System.out.println ("Hello World");
 	 }
  }
