c6a3df657e34a677e9af298dd62a33f5287c4e41
ArtemKanevskii <nwoa@mail.ru>
2018-11-13 12:05:35 +0200

    1.4. JUnit[#85217]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -14,6 +14,13 @@
 	 public static void main(String[] args) {
 
 		 System.out.println ("Hello World");
-	 }
- }
- 
+	}
+ /**
+ * Method echo.
+ * @param name Your name.
+ * @return Echo plus your name.
+ */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Artem Kanevskii
+* @version 0.1
+* @since 12.10.2018
+*/
+package ru.job4j.calculate;
