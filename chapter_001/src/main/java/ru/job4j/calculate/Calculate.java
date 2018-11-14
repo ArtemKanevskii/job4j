165f172ccda642d1ffd3931f1f9a85387c467c24
ArtemKanevskii <nwoa@mail.ru>
2018-11-14 14:17:01 +0200

    Поправлено форматирование в файлах проекта

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,24 +1,24 @@
- package ru.job4j.calculate;
- /**
+package ru.job4j.calculate;
+/**
  * Class Calculate 
- * 
+ *
  * Вывод заданной строки в консоль.
  * @author Artem Kanevskii
  * @since 12.11.2018
  * @version 0.1
  */
- public class Calculate {
- /**
- * @param args - args.
- */	 
-	 public static void main(String[] args) {
+public class Calculate {
+	/**
+	 * @param args - args.
+	 */
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
- /**
- * Method echo.
- * @param name Your name.
- * @return Echo plus your name.
- */
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -1,28 +1,28 @@
- package ru.job4j.calculate;
+package ru.job4j.calculate;
 
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
- /**
+/**
  * Class CalculateTest.
- * 
+ *
  * Тестирование вывода заданной строки в консоль.
  * @author Artem Kanevskii
  * @since 12.11.2018
  * @version 0.1
  */
- public class CalculateTest {
- /**
- * Test echo.
- */
-@Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Artem Kanevskii";
-    String expect = "Echo, echo, echo : Artem Kanevskii"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+public class CalculateTest {
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Artem Kanevskii";
+        String expect = "Echo, echo, echo : Artem Kanevskii";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
