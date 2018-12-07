c891e3e87203ac723b4b1ec992b807ad20fe6202
ArtemKanevskii <nwoa@mail.ru>
2018-12-07 10:45:47 +0200

    5.5. Рефакторинг кода.[#85186]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,61 +1,41 @@
 package ru.job4j.loop;
+import java.util.function.BiPredicate;
 
 /**
  * @author Artem Kanevskii
- * @version 0.1
- * @since 30.11.2018
+ * @version 0.2
+ * @since 07.12.2018
  */
 public class Paint {
-    /**
-     * Метод строит правую сторону треугольника.
-     * @param height - высота треугольника.
-     * @return возврашает правую сторону треугольника.
-     */
     public String rightTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int weight = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
     }
-    /**
-     * Метод строит левую сторону треугольника.
-     * @param height - высота треугольника.
-     * @return возврашает левую сторону треугольника.
-     */
+
     public String leftTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int weight = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= weight - column - 1) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
     }
-    /**
-     * Строит пирамиду в псевдографике из символа ^ и пробелов.
-     * @param height - высота пирамиды.
-     */
+
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
